11d5bb557306fc97b967ff5ba836311b1a2be6e8
Oskar Mokrzycki <oskar.mokrzycki@gmail.com>
2022-06-28 13:03:39 +0200

    generate method with formactionresult (todo: session handling)

--- BarcodeScanner/BarcodeScanner/PFTest.cs
+++ BarcodeScanner/BarcodeScanner/PFTest.cs
@@ -4,6 +4,7 @@ using Soneta.Business.App;
 using Soneta.Business.UI;
 using Soneta.CRM;
 using Soneta.Handel;
+using Soneta.Magazyny;
 using Soneta.Towary;
 using System;
 using System.Collections.Generic;
@@ -11,26 +12,21 @@ using System.Linq;
 
 namespace BarcodeScanner
 {
-    public class PFTest : INewBarCode
+    public class PFTest : ContextBase, INewBarCode
     {
         private Item _focusedItem;
         private Item[] _selectedItems;
 
-        public PFTest() //: base(context)
+        public PFTest(Context context) : base(context) 
         {
             Items = new List<Item>();
 
-            Items.Add(new Item("05022087001", "022087 ZESTAW KLUCZY TRZPIENIOWYCH SW1.5-10", "4013288104816"));
-            Items.Add(new Item("1655892", "0513.15 KLUCZ PŁASKI 13X15 /CAROLUS/", "4036548513159"));
+            Items.Add(new Item("05022087001", "022087 ZESTAW KLUCZY TRZPIENIOWYCH SW1.5-10", "4013288104816", context));
+            Items.Add(new Item("1655892", "0513.15 KLUCZ PŁASKI 13X15 /CAROLUS/", "4036548513159", context));
         }
 
         [Context]
-        public Session Session { get; set; }
-
-        [Context]
         public Login Login { get; set; }
-        [Context]
-        public Context Context { get; set; }
 
         public Item[] SelectedItems
         {
@@ -38,6 +34,7 @@ namespace BarcodeScanner
             set
             {
                 _selectedItems = value;
+                if(_selectedItems != null)
                 Context.Set(_selectedItems);
             }
         }
@@ -47,10 +44,13 @@ namespace BarcodeScanner
             set
             {
                 _focusedItem = value;
+                if(_focusedItem != null)
                 Context.Set(_focusedItem);
             }
         }
         public string Code { get; set; }
+        public Kontrahent Kontrahent { get; set; }
+        public Magazyn Magazyn { get; set; }
         public List<Item> Items { get; set; }
 
 
@@ -60,34 +60,33 @@ namespace BarcodeScanner
             Context.Session.InvokeChanged();
         }
 
-        private HandelModule GetHandelModule()
+        private HandelModule GetHandelModule(Session session)
         {
-            return Context.Session.GetHandel();
+            return HandelModule.GetInstance(session);
         }
-        private TowaryModule GetTowaryModule()
+        private TowaryModule GetTowaryModule(Session session)
         {
-            return Context.Session.GetTowary();
+            return TowaryModule.GetInstance(session);
         }
-        
+
         public object GeneratePZ()
         {
-            return new MessageBoxInformation("Czy wykonać operację ?")
+            return new MessageBoxInformation("Czy wygenerować PZ?")
             {
-                Text = "Opis operacji",
+                Text = "Potwierdź wygenerowanie PZ",
                 YesHandler = () =>
                 {
-                    using(Session session = Login.CreateSession(false, false))
+                    using (Session session = Login.CreateSession(false, false))
                     {
                         using (ITransaction t = session.Logout(true))
                         {
-                            var handelModule = HandelModule.GetInstance(session);
-                            var towaryModule = TowaryModule.GetInstance(session);
+                            var handelModule = GetHandelModule(session);
+                            var towaryModule = GetTowaryModule(session);
                             DokumentHandlowy dh = new DokumentHandlowy();
                             handelModule.DokHandlowe.AddRow(dh);
                             dh.Definicja = handelModule.DefDokHandlowych.PrzyjęcieMagazynowe2;
-                            dh.Magazyn = handelModule.Magazyny.Magazyny.Firma;
-                            dh.Numer.NumerPelny = "123456";
-                            dh.Kontrahent = session.GetCRM().Kontrahenci.Incydentalny;
+                            dh.Magazyn = Magazyn;
+                            dh.Kontrahent = Kontrahent;
 
                             foreach (var item in Items)
                             {
@@ -100,86 +99,94 @@ namespace BarcodeScanner
                         session.Save();
                     }
                     
-                    return "git";
+                    return "Dokument PZ został wygenerowany.";
                 },
                 NoHandler = () => "Operacja przerwana"
             };
-
-            
-
         }
-        public object GeneratePZDifferentWay()
-        {
-            DokumentHandlowy dh = new DokumentHandlowy();
-            using (Session session = Login.CreateSession(false, false))
-            {
-                using (ITransaction t = session.Logout(true))
-                {
-                    var handelModule = HandelModule.GetInstance(session);
-                    var towaryModule = TowaryModule.GetInstance(session);
-                    
-                    handelModule.DokHandlowe.AddRow(dh);
-                    dh.Definicja = handelModule.DefDokHandlowych.PrzyjęcieMagazynowe2;
-                    dh.Magazyn = handelModule.Magazyny.Magazyny.Firma;
-                    
-                    dh.Kontrahent = session.GetCRM().Kontrahenci.Incydentalny;
 
-                    foreach (var item in Items)
-                    {
-                        PozycjaDokHandlowego pozDH = new PozycjaDokHandlowego(dh);
-                        handelModule.PozycjeDokHan.AddRow(pozDH);
-                        pozDH.Towar = towaryModule.Towary.WgKodu[item.Kod];
-                    }
-                    t.Commit();
-                    return new FormActionResult
-                    {
-                        EditValue = dh,
-                        UseDialog = true,
-                        //CommittedHandler = (cx) =>
-                        //{
-                        //    return DBNull.Value;
-                        //}
-                    };
+        public object GeneratePZWithForm()
+        {
+            if(Session.CurrentTransaction != null)
+            Session.CurrentTransaction.Dispose();
+            
+            using (ITransaction t = Session.Logout(true))
+            {
+                var handelModule = GetHandelModule(Session);
+                var towaryModule = GetTowaryModule(Session);
+                DokumentHandlowy dh = new DokumentHandlowy();
+                handelModule.DokHandlowe.AddRow(dh);
+                dh.Definicja = handelModule.DefDokHandlowych.PrzyjęcieMagazynowe2;
+                dh.Magazyn = Magazyn;
+                dh.Kontrahent = Kontrahent;
+
+                foreach (var item in Items)
+                {
+                    PozycjaDokHandlowego pozDH = new PozycjaDokHandlowego(dh);
+                    handelModule.PozycjeDokHan.AddRow(pozDH);
+                    pozDH.Towar = towaryModule.Towary.WgKodu[item.Kod];
+                    pozDH.Ilosc = item.Qty;
                 }
-                session.Save();
+                t.Commit();
+                
+                return new FormActionResult
+                {
+                    EditValue = dh,
+                    CommittedHandler = ctx =>
+                    {
+                        return new MessageBoxInformation("Operacja zakończona");
+                    }
+                };
             }
             
-
         }
+
         public object Enter(Context cx, string code, double quantity)
         {
             Code = code;
             Towar towar = cx.Session.GetTowary().Towary.WgEAN[code].GetFirst();
-            Items.Add(new Item(towar.Kod, towar.Nazwa, towar.EAN));
+            Items.Add(new Item(towar.Kod, towar.Nazwa, towar.EAN, cx));
             //OnChanged();
 
             cx.Session.InvokeChanged();
 
             return DBNull.Value;
         }
-
-
     }
-    public class Item
+    public class Item : ContextBase
     {
-        public Item()
+        private Quantity _qty = new Quantity(1.0);
+
+        public Item(Context cx) : base(cx)
         {
+            OnChanged(EventArgs.Empty);
         }
 
-        public Item(string kod, string nazwa, string eAN)
+        public Item(string kod, string nazwa, string eAN, Context cx) : base (cx)
         {
             Kod = kod;
             Nazwa = nazwa;
             EAN = eAN;
+
+            OnChanged(EventArgs.Empty);
         }
 
         public string Kod { get; set; }
         public string Nazwa { get; set; }
         public string EAN { get; set; }
-        public decimal Qty { get; set; } = 1.0M;
+        public Quantity Qty
+        {
+            get => _qty;
+            set
+            {
+                _qty = value;
+                OnChanged(EventArgs.Empty);
+            }
+        }
     }
-
 }
+
+
 
 
 
